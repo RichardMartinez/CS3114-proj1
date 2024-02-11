86f4b19c3c35a480ab2c8d63f2759453be2c9120
RichardMartinez <rmartinez21@vt.edu>
2024-02-10 23:08:03 -0500

    add some commandprocessor tests

--- S24_P1SkipList_starterkit/src/CommandProcessorTest.java
+++ S24_P1SkipList_starterkit/src/CommandProcessorTest.java
@@ -13,6 +13,8 @@ import student.TestableRandom;
  */
 public class CommandProcessorTest extends TestCase {
     
+    private CommandProcessor cmdProc;
+    
     /**
      * The setUp() method will be called automatically before
      * each test and reset whatever the test modified. For this
@@ -20,9 +22,43 @@ public class CommandProcessorTest extends TestCase {
      * creat a database here for use in each test case.
      */
     public void setUp() {
-    	// TODO: implement setup
+    	cmdProc = new CommandProcessor();
     }
     
-    // TODO: implement tests
+    public void testInsert() {
+        // Valid insert
+        systemOut().clearHistory();
+        cmdProc.processor("insert a 1 0 2 4");
+        String expectedOutput = "Rectangle inserted: (a, 1, 0, 2, 4)";
+        String actualOutput = systemOut().getHistory();
+        assertFuzzyEquals(expectedOutput, actualOutput);
+        
+        // Invalid insert
+        systemOut().clearHistory();
+        cmdProc.processor("insert b -1 -1 -1 -1");
+        expectedOutput = "Rectangle rejected: (b, -1, -1, -1, -1)";
+        actualOutput = systemOut().getHistory();
+        assertFuzzyEquals(expectedOutput, actualOutput);
+    }
+    
+    public void testDump() {
+        // Make next levels: 3, 2
+        TestableRandom.setNextBooleans(true, true, false, true, false);
+        
+        cmdProc.processor("insert a 1 0 2 4");
+        cmdProc.processor("insert b 2 0 4 8");
+               
+        String expectedOutput = "SkipList dump:\n"
+            + "Node with depth 3, Value null\n"
+            + "Node with depth 3, Value (a, 1, 0, 2, 4)\n"
+            + "Node with depth 2, Value (b, 2, 0, 4, 8)\n"
+            + "SkipList size is: 2\n";
+        
+        systemOut().clearHistory();
+        cmdProc.processor("dump");
+        
+        String actualOutput = systemOut().getHistory();
+        assertFuzzyEquals(expectedOutput, actualOutput);
+    }
 
 }
